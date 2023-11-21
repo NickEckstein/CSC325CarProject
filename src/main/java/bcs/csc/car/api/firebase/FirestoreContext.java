07f41808871a2aab4f9a01500aecbd493be604d3
Brian Niski <50961607+niskb@users.noreply.github.com>
2023-11-21 14:10:49 -0500

    Firebase Lab - Firebase with JavaFX FXML Maven Support

---
+++ src/main/java/bcs/csc/car/api/firebase/FirestoreContext.java
@@ -0,0 +1,33 @@
+package bcs.csc.car.api.firebase;
+
+import bcs.csc.car.api.App;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FirestoreContext {
+
+    public Firestore firebase() {
+        FileInputStream serviceAccount;
+        FirebaseOptions options;
+        try {
+            serviceAccount = new FileInputStream(App.RESOURCES_PATH + "key.json");
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+            System.out.println("SUCCESSFULLY CONNECTED TO FIREBASE");
+        } catch (FileNotFoundException ex) {
+            System.out.println("UNABLE TO CONNECT TO FIREBASE \"key.json\" DOES NOT EXIST");
+        } catch (IOException ex) {
+            System.out.println("FAILURE TO CONNECT TO FIREBASE");
+        }
+        return FirestoreClient.getFirestore();
+    }
+
+}
