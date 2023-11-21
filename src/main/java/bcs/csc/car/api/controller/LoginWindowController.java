6a5832b7622bcafbbbbd73d7937e2bd5187f6cc1
soblab <soblab@Whit209-24.it.farmingdale.edu>
2023-11-21 13:24:47 -0500

    Login View Save 1:27pm 11/21/2023

--- src/main/java/bcs/csc/car/api/controller/LoginWindowController.java
+++ src/main/java/bcs/csc/car/api/controller/LoginWindowController.java
@@ -7,14 +7,47 @@ package bcs.csc.car.api.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author itlabs
+ * @author Meraj Ali
  */
 public class LoginWindowController implements Initializable {
+    
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button loginButton;
+    
+    
+    String username;
+    String password;
+    
+
+    @FXML
+    void onClickLoginPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verifyPassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verifyUsername(ActionEvent event) {
+
+    } 
 
     /**
      * Initializes the controller class.
@@ -22,6 +55,7 @@ public class LoginWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
     
 }
