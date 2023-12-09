82429932e4a0fa8e596f20bb147936693f8781be
Brian Niski <50961607+niskb@users.noreply.github.com>
2023-12-09 15:11:38 -0500

    Remove conflicts that must be resolved
    For merging into main

--- src/main/java/bcs/csc/car/api/App.java
+++ src/main/java/bcs/csc/car/api/App.java
@@ -15,6 +15,10 @@ import bcs.csc.car.api.sql.model.transmission.Transmission;
 import bcs.csc.car.api.sql.model.vehicle.VehicleType;
 import bcs.csc.car.api.sql.utils.DataParser;
 import bcs.csc.car.api.sql.utils.SQLiteUtils;
+import bcs.csc.car.api.firebase.FirestoreContext;
+import bcs.csc.car.api.firebase.model.SampleUser;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.auth.FirebaseAuth;
 import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -25,18 +29,48 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.LinkedList;
 
+/**
+ *
+ * @authors Meraj Ali, Nicholas Eckstein, Brian Niski, Bryant Velasquez
+ */
 public class App extends Application {
 
+    /**
+     * File Access Strings
+     */
     public static final String VIEW_PATH = "view/";
+    public static final String RESOURCES_PATH = "./src/main/resources/";
+    public static final String LOAD_FXML_FILE_NAME = "sampleFirebase";
+
+    /**
+     * App Objects
+     */
     private static Scene scene;
     public static LinkedList<LegalMake_Model> legalMake_ModelList = new LinkedList<>();
 
+    /**
+     * Firebase Objects
+     */
+    public static Firestore fstore;
+    public static FirebaseAuth fauth;
+    private final FirestoreContext contxtFirebase = new FirestoreContext();
+
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML(VIEW_PATH + "vehicleTable"));
-        stage.setTitle("Mini Car Project");
+        fstore = contxtFirebase.firebase();
+        fauth = FirebaseAuth.getInstance();
+
+        scene = new Scene(loadFXML(VIEW_PATH + LOAD_FXML_FILE_NAME));
+        stage.setTitle("Firebase Test Suite");
         stage.setScene(scene);
         stage.show();
+        
+//        System.out.println("Add new user to Firebase");
+//        SampleUser.addUser(new SampleUser("test@email.com", "averysecurepassword"));
+//        SampleUser.addUser(new SampleUser("test2@email.com", "averysecurepassword2"));
+        System.out.println("Success");
+        
+        SampleUser.readUsers();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -92,9 +126,11 @@ public class App extends Application {
             DataParser.saveFile(legalMake_ModelList, "Legal Make_Model List");
 
             App.legalMake_ModelList = legalMake_ModelList;
+            System.out.println("CREATED PROJECT FILES SUCCESSFULLY");
         } else {
             LinkedList<LegalMake_Model> legalMake_ModelList = DataParser.readFile("Legal Make_Model List.dat");
             App.legalMake_ModelList = legalMake_ModelList;
+            System.out.println("LOADED LEGAL MAKE_MODEL FILE SUCCESSFULLY");
         }
         launch();
     }
